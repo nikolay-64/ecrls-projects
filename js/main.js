1240a5089161a4d38f47892606fbd1f657fa77d0
nikolay-64 <kolyavyazmin@yandex.ru>
2021-10-03 14:02:44 +0400

    add slide owl-caruosel2 for project

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    // $("#product-carousel").owlCarousel({
+    //     items:1,
+    //     loop: true,
+    //     nav: true,
+    //     navText: ['Prev', 'Next'],
+    // });
+
+    // Находим  элемент в котором будет инициализирована каруседль
+    const productCarousel = $("#product-carousel");
+    const productCarousel1 = $("#product-carousel-1");
+
+    // Создаем карусель в элементе #product-carousel
+    productCarousel.owlCarousel({
+        items:1,
+        loop: true,
+        // nav: true,
+        // navText: ['Prev', 'Next'],
+    })
+
+    // Находим кнопки назад и вперед
+    
+    const buttonPrev = $('.nav-button-left');
+    const buttonNext = $('.nav-button-rigth');
+
+    buttonPrev.click(function () {
+        productCarousel.trigger('prev.owl.carousel');
+        productCarousel1.trigger('prev.owl.carousel');
+    });
+
+    buttonNext.click(function () {
+        productCarousel.trigger('next.owl.carousel');
+        productCarousel1.trigger('next.owl.carousel');
+    });
+
+
+    // Создаем карусель в элементе #product-carousel-1
+    productCarousel1.owlCarousel({
+        items:1,
+        loop: true,
+    });
+  });
